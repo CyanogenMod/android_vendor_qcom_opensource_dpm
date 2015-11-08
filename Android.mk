070a9ec1127b42da4a4c58bd865ba64b8730d9e3
Steve Kondik <steve@cyngn.com>
2015-11-07 17:39:35 -0800

    dpm: Get tcmiface off the boot classpath
    Change-Id: I9fba2bbf4932dbe3627f2d390540a5d7680086cd

--- Android.mk
+++ Android.mk
@@ -6,4 +6,12 @@ LOCAL_NO_STANDARD_LIBRARIES := true
 LOCAL_MODULE:= tcmiface
 LOCAL_JAVA_LIBRARIES := core-libart
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
-include $(BUILD_JAVA_LIBRARY)
+include $(BUILD_STATIC_JAVA_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_NO_STANDARD_LIBRARIES := true
+LOCAL_MODULE:= tcmiface-hostdex
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
+include $(BUILD_HOST_DALVIK_JAVA_LIBRARY)
