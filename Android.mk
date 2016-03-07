cdef3f13f2f45abe74b1bd4b6d715a088c410399
David Ng <dave@codeaurora.org>
2016-03-07 15:54:08 -0800

    Enable JACK support
    Remove explicit disabling of standard Java library linkage
    to support JACK compilation.
    
    Change-Id: Ic504aeeb34bfd29f8174e0abca232b79a8d95280

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_NO_STANDARD_LIBRARIES := true
+#LOCAL_NO_STANDARD_LIBRARIES := true
 LOCAL_MODULE:= tcmiface
 LOCAL_JAVA_LIBRARIES := core-libart
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
