25df0014dc59c7d1f2859679bbeba3e95bb404c9
Sharat Masetty <smasetty@codeaurora.org>
2016-03-24 16:12:22 +0530

    Enable standard Java Library Linkage.
    Enabling  standard Java library linkage and added
    support to JACK compilation.
    
    Change-Id: I5acf33f2d09a1ba13d9305dea00432f80f18f12f

--- Android.mk
+++ Android.mk
@@ -2,9 +2,9 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-#LOCAL_NO_STANDARD_LIBRARIES := true
+LOCAL_NO_STANDARD_LIBRARIES := true
 LOCAL_MODULE:= tcmiface
-LOCAL_JAVA_LIBRARIES := core-libart
+LOCAL_JAVA_LIBRARIES := core-oj core-libart
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 include $(BUILD_STATIC_JAVA_LIBRARY)
 
